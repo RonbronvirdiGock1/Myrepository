8d1c732f4e2ee61bb6f9a4a17d06b07cf4241ace
RonbronvirdiGock1 <tejasmail82@gmail.com>
2025-09-09 20:18:38 +1000

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,10 @@
+print("welcome to vs code")
+myname="jash"
+my_age= 12
+h1= 179.5
+print("my name is ",myname)
+print("my age is ",my_age)
+print(f"my name is {myname} and my height is  {h1}")
+print(4)
+names=input("enter your name")
+print(names)

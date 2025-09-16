ec00abb789f873974420d2e04d83c5b6be5d14d8
RonbronvirdiGock1 <tejasmail82@gmail.com>
2025-09-16 21:17:56 +1000

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,16 @@
+print("Hello friends, tell me your birthdates")
+myname1="Avyaan"
+my_birthday="January 4"
+print(f"My name is {myname1} and my birthday is on {my_birthday}")
+myname2="Atharva"
+my_birthday="September 16"
+print(f"My name is {myname2} and my birthday is on {my_birthday}")
+myname3="Will"
+my_birthday="December 3"
+print(f"My name is {myname3} and my birthday is on {my_birthday}")
+myname4="Eric"
+my_birthday="November 12"
+print(f"My name is {myname4} and my birthday is on {my_birthday}")
+myname5="Artin"
+my_birthday="September 11"
+print(f"My name is {myname5} and my birthday is on {my_birthday}")

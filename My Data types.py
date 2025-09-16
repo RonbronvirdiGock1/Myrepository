2af513226f893a9968455594a5d9223dc3a37230
RonbronvirdiGock1 <tejasmail82@gmail.com>
2025-09-16 19:46:04 +1000

    Add files via upload

---
+++ My Data types.py
@@ -0,0 +1,12 @@
+a= 5
+b= 2.5
+c= "coding"
+d= False
+print("types of a is",type(a))
+print(a)
+print("type of b is",type(b))
+print("the value of b is",(b))
+print("type of c is",type(c))
+print(c)
+print("type of d is",type(d))
+print("the value of d is",(d))

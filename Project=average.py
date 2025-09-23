d39075b897da279c803b185230c97d785ab194a9
RonbronvirdiGock1 <tejasmail82@gmail.com>
2025-09-23 19:43:13 +1000

    Add files via upload

---
+++ Project=average.py
@@ -0,0 +1,5 @@
+marksfor_1=int(input("enter the marks for math"))
+marksfor_2=int(input("enter the marks for science"))
+marksfor_3=int(input("enter the marks for history"))
+average=((marksfor_1+marksfor_2+marksfor_3)//300)*100
+print("average of three marks ",average)

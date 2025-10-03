2713f79158d396b3521bc595234979125cc26022
RonbronvirdiGock1 <tejasmail82@gmail.com>
2025-10-03 19:05:03 +1000

    Add files via upload

---
+++ Homework Summer times.py
@@ -0,0 +1,6 @@
+weather_1= 30
+if weather_1 > 0:
+    print(weather_1,"is warmer so you should wear summer clothes")
+weather_2= -2
+if weather_2 < 0:
+    print(weather_2,"is cold so you should wear winter clothes")

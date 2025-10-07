3c7f1e58028c1ba8cc8efce6104f0ad206dee77d
RonbronvirdiGock1 <tejasmail82@gmail.com>
2025-10-07 20:33:47 +1100

    Add files via upload

---
+++ Python operators II.py
@@ -0,0 +1,18 @@
+a = 18
+b = 45
+c = 0
+
+if a and b and c:
+    print("All the numbers have same boolean value as true")
+else:
+    print("Aleast one number has boolean value as false")
+
+if a > 0 or b > 0:
+    print("Either of the numbers is greater than 0")
+else:
+    print("No number is greater than 0")
+
+if b > 0 or c == 0:
+    print("Either number is greater than or equal to 0")
+else:
+    print("No number is greater than 0")

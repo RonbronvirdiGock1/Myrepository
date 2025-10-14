68b86e4e09670e8a30ef97b7b751556ee36474fa
RonbronvirdiGock1 <tejasmail82@gmail.com>
2025-10-14 20:16:25 +1100

    Add files via upload

---
+++ Python Chalenges.py
@@ -0,0 +1,6 @@
+x = 23
+y = 67
+w = 18
+z = 9
+value = (x + y)*z - w
+print(" the value of (x + y)**z - w is", value)
---
+++ Python Challenges 2.0.py
@@ -0,0 +1,6 @@
+name = "Jash"
+age = 13
+if name  == "Jas" or name == "Tejas" and age >= 45 :
+    print("Hello! Welcome")
+else :
+    print("Goodbye !!")

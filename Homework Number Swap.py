62ddd65423adf4d58eb34fe3bdc8466a5d578a17
RonbronvirdiGock1 <tejasmail82@gmail.com>
2025-10-26 19:34:19 +1100

    Add files via upload

---
+++ Homework Number Swap.py
@@ -0,0 +1,23 @@
+a = int(input("Enter first number (a): "))
+b = int(input("Enter second number (b): "))
+c = int(input("Enter third number (c): "))
+
+print("\nBefore swapping:")
+print("a =", a, "b =", b, "c =", c)
+
+a, b, c = b, c, a
+
+print("\nAfter swapping:")
+print("a =", a, "b =", b, "c =", c)
+
+a = int(input("Enter first number (a): "))
+b = int(input("Enter second number (b): "))
+c = int(input("Enter third number (c): "))
+
+print("\nBefore swapping:")
+print("a =", a, "b =", b, "c =", c)
+
+a, b, c = c, b, a
+
+print("\nAfter swapping:")
+print("a =", a, "b =", b, "c =", c)

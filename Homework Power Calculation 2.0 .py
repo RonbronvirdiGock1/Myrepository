4c8703437e7a0eb8d0dd6bfa449359f4da73c3a6
RonbronvirdiGock1 <tejasmail82@gmail.com>
2025-11-18 16:35:05 +1100

    Add files via upload

---
+++ Homework Power Calculation 2.0 .py
@@ -0,0 +1,6 @@
+base = float(input("Enter the base number (e.g., 2): "))
+exponent = float(input("Enter the exponent (e.g., 3): "))
+
+result = base ** exponent
+
+print(f"\n{base} raised to the power of {exponent} is: {result}")

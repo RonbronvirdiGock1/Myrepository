d5c1eae26d67151b7007ba3d5128c3b91c2a8bce
RonbronvirdiGock1 <tejasmail82@gmail.com>
2025-12-02 16:07:29 +1100

    Add files via upload

---
+++ Homework Power Calculation 3.0 .py
@@ -0,0 +1,6 @@
+Number1 = float(input("Enter a number (e.g., 2): "))
+power = float(input("Enter a power (e.g., 3): "))
+
+for i in range(1,4):
+    result = power * Number1
+print(f"\n{Number1} raised to the power of {power} is: {result}")
